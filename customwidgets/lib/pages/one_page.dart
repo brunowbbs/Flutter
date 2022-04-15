61f228122123efb27741433cf59549c589123927
Wesley Bruno <engwesleybruno@gmail.com>
2022-04-15 14:54:20 -0300

    custom widgets

---
+++ customwidgets/lib/pages/one_page.dart
@@ -0,0 +1,29 @@
+import 'package:customwidgets/widgets/custom_button_widget.dart';
+import 'package:flutter/material.dart';
+
+class OnePage extends StatelessWidget{
+  const OnePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Page 1"),
+      ),
+
+      body: Center(
+        child: CustomButtonWidget(
+          titleSize: 20,
+          disabled: false,
+          onPressed: (){ 
+            print('ok');
+          }, 
+          title: "Pressione"
+        ),
+      ),
+    );
+
+  }
+
+}
